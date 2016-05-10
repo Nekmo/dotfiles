d872c0ea908540109d0564d966024c56ea4a4c20
nekmo <nekmo@alsur.es>
2016-05-10 21:45:00 +0200

    Quitado blink del welcome cuando se accede como root.

--- bashrc.d/hello.bash
+++ bashrc.d/hello.bash
@@ -9,5 +9,5 @@ uptime_now=`uptime | sed -r 's/,  [0-9]+ users.+//' | sed -r 's/.+ up //' | sed 
 echo -e "Bienvenido a ${c_host}${HOSTNAME}${c_null} ${c_dark_gray}(Ver.${c_null} "`uname -r`" ${c_dark_gray}IP.${c_null} $IP${c_dark_gray}).${c_null}"
 echo -e "${c_dark_gray}RAM:${c_null} $rusedram/$rtotalram ${c_dark_gray}CPU:${c_null} "`cpupcnt`"% ${c_dark_gray}Uptime:${c_null} $uptime_now. Escriba ${c_dark_gray}Assistance${c_null} para ayuda."
 if [ `whoami` == 'root' ]; then
-    echo -e "${c_bold}${c_yellow}Atención:${c_null}${c_bold} estás accediendo como super-usuario. ${c_blink}Tenga precaución, amigo administrador.${c_null}"
+    echo -e "${c_bold}${c_yellow}Atención:${c_null}${c_bold} estás accediendo como super-usuario. ${c_yellow}Tenga precaución, amigo administrador.${c_null}"
 fi
