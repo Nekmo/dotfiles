a1a85d6cb4b368694fe17bf7d232e572c6e2680a
nekmo <nekmo@alsur.es>
2016-07-08 19:24:32 +0200

    $DOTFILES_DIR in custom.

--- bashrc.d/custom.bash
+++ bashrc.d/custom.bash
@@ -1,17 +1,17 @@
 #-------------------------------------------------------------
 # Scripts personalizados
 #-------------------------------------------------------------
-_custom="$HOME/dotfiles/bashrc.d/custom/${HOSTNAME}.bash"
+_custom="$DOTFILES_DIR/bashrc.d/custom/${HOSTNAME}.bash"
 if [ -a "$_custom" ]; then
 	source "$_custom"
 fi
 
-_is_desktop="$HOME/dotfiles/bashrc.d/custom/is_desktop.bash"
+_is_desktop="$DOTFILES_DIR/bashrc.d/custom/is_desktop.bash"
 if [[ -a "$_is_desktop" && !$is_server ]]; then
 	source "$_is_desktop"
 fi
 
-_is_server="$HOME/dotfiles/bashrc.d/custom/is_server.bash"
+_is_server="$DOTFILES_DIR/bashrc.d/custom/is_server.bash"
 if [[ -a "$_is_server" && $is_server ]]; then
 	source "$_is_server"
 fi
